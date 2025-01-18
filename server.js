adcec3da69f7d8eb23590a52edbf61515e706183
mearif12 <mearifurahman@outlook.com>
2025-01-18 22:24:38 +0600

    simple express greeting app

---
+++ server.js
@@ -0,0 +1,41 @@
+require('ejs');
+const express = require('express');
+const app = express();
+
+const port = 4500;
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+app.listen(port,()=>{
+    console.log(`The server is running at http://localhost:${port}`);
+});
+
+app.get("/",(req,res)=>{
+    res.status(200).render("home",{
+        imgSrc:"welcome.png",
+        message:"Enter your name",
+        fname:null
+    });
+});
+
+let msg,newImg;
+app.post("/greet",(req,res)=>{
+    const name = req.body.name;
+    msg = name;
+    if(msg === ""){
+        newImg = "welcome.png";
+    } else {
+        newImg = "batHello.png";
+    }
+    res.status(201).redirect("/greet");
+});
+
+app.get("/greet",(req,res)=>{
+    res.status(200).render("home",{
+        message:"Enter your name",
+        imgSrc: newImg,
+        fname:msg
+    });
+});
